28cf017f5a061f25188674050002dac17b1d6e0e
rbarbazz <raphael.barbazza@gmail.com>
2018-08-01 01:48:43 +0200

    clang complete

--- include/lemin.h
+++ include/lemin.h
@@ -6,7 +6,7 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 00:00:37 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/07/31 01:05:45 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/08/01 00:58:19 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,18 @@
 # include "libft.h"
 # include "ft_printf.h"
 # include "get_next_line.h"
+
+typedef struct		s_room
+{
+	int				num;
+	char			*name;
+	struct s_room	*next;
+}					t_room;
+
+typedef struct		s_lem
+{
+	int				nb_ants;
+	t_room			*room;
+}					t_lem;
 
 #endif
