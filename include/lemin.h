2d54d381eb62e6063d7fade8004f9f015370ff72
rbarbazz <raphael.barbazza@gmail.com>
2018-08-01 03:33:32 +0200

    struct added

--- include/lemin.h
+++ include/lemin.h
@@ -6,15 +6,13 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/31 00:00:37 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/08/01 00:58:19 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/08/01 02:50:22 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEMIN_H
 # define LEMIN_H
 # include "libft.h"
-# include "ft_printf.h"
-# include "get_next_line.h"
 
 typedef struct		s_room
 {
@@ -26,7 +24,13 @@ typedef struct		s_room
 typedef struct		s_lem
 {
 	int				nb_ants;
-	t_room			*room;
+	t_room			*start_room;
 }					t_lem;
+
+/*
+** parsing
+*/
+
+t_lem		*get_lem(void);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,14 +6,30 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 13:22:39 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/07/30 23:57:18 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/08/01 02:52:54 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
+t_lem		*get_lem(void)
+{
+	static t_lem	lem;
+
+	return(&lem);
+}
+
+static int	store_map()
+{
+	int		ret;
+	char	*buf;
+
+	while ((ret = read(STDIN_FILENO,
+}
+
 int			main(void)
 {
-	ft_printf("Hello");
+	if (store_map())
+		return(ft_printf("ERROR"));
 	return (0);
 }
